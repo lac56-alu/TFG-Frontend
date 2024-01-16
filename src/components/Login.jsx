ef6ea26e730a9b6c5f9e26d1671e925a12b78c85
Luís Alfonso Culiáñez <lac56@mscloud.ua.es>
2024-01-16 13:01:50 +0100

    Initialized login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,25 +10,34 @@ function Login() {
           <Navbar />
         </div>
       </div>
-      <div>
-        <form>
-            <div className='flex flex-row justify-between items-center w-full'>
-                <label htmlFor='email' className='text-white'>Email</label>
-                <input type='email' placeholder='Inserta Email'></input>
-            </div>
-            <div className='flex flex-row justify-between items-center w-full'>
-                <label htmlFor='password' className='text-white'>Contraseña</label>
-                <input type='password' placeholder='Inserta Contraseña'></input>
-            </div>
-            <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] 
-                                      text-primary outline-none ${styles} rounded-[10px]`}>
-                Entrar
-            </button>
 
-            <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] 
-                                      text-primary outline-none ${styles} rounded-[10px]`}>
-                Registrate
-            </button>
+      <div className={`${styles.marginY} ${styles.padding} bg-black-gradient-2 rounded-[20px] box-shadow`} >
+        <h2 className={styles.heading2}>
+          Inicio Sesión
+        </h2>
+
+        <form>
+          <div className='flex flex-row items-center'>
+            <label className="font-medium text-[18px] font-poppins text-white">
+              Email 
+            </label>
+            <input type='email' className='py-2 px-10 font-poppins rounded-[10px] ml-2' placeholder='Inserta Email'></input>
+          </div>
+
+          <div className='flex flex-row items-center mt-3'>
+            <label className="font-medium text-[18px] font-poppins text-white">
+              Contraseña 
+            </label>
+            <input type='password' className='py-2 px-10 font-poppins rounded-[10px] ml-2' placeholder='Inserta Contraseña'></input>
+          </div>
+          
+          <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] mt-2`}>
+            Entrar
+          </button>
+
+          <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
+            Registrate
+          </button>
         </form>
       </div>
     </div>
